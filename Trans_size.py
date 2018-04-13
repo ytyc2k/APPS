4131c1d9af8e62ee91a11848dff161794b139882
ytyc2k <ytyc2k@gmail.com>
2018-04-12 19:43:43 -0500

    2017.4.12 7:43pm

--- Trans_size.py
+++ Trans_size.py
@@ -9,9 +9,9 @@ class win(QWidget):
         self.A=A
         self.B=B
         lay=QGridLayout()
-        for i in range(6):
+        for i in range(14):
             self.le.append(QLineEdit())
-            lay.addWidget(self.le[i],[i,i-1][i%2],i%2)
+            lay.addWidget(self.le[i], i / 3, i % 3)
         self.le[0].setText(str(self.A) + 'm')
         self.le[1].setText(str(self.B) + 'm')
         self.t3 = QTextEdit()
@@ -19,16 +19,16 @@ class win(QWidget):
         lay.addWidget(b1,i+3,0)
         lay.addWidget(b2,i+3,1)
         self.setLayout(lay)
-        b1.clicked.connect(self.aaa)
+        b1.clicked.connect(self.trans)
         b2.clicked.connect(self.close)
 
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_Escape:
             self.close()
-
-    def aaa(self):
+    def trans(self):
         self.le[2].setText(str('%5.2f'%(self.A*self.B))+'m2')
-        self.le[3].setText(str(self.B*100)+'cm')
+        self.le[3].setText(str(self.A*self.B*10.76)+'sqft')
+        self.le[4].setText(str(self.B*100)+'cm')
 
 app = QApplication([])
 w = win(5.2,2.6)
