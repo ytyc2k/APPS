05200daae4c1902032effb69b914527b4fe10867
ytyc2k <ytyc2k@gmail.com>
2018-05-21 00:08:48 -0500

    2017.4.12 7:43pm

--- TypingGame.py
+++ TypingGame.py
@@ -5,13 +5,14 @@ import random
 # lists of English words and Chinese translations which you defined
 wordstr='salad,jammy,pastry,blender,mustard,doughnut,crust,onion,ketchup,pudding,aperitif,caviar,pie,broth,cheese'
 hzstr='色拉，涂果酱的，酥皮点心，搅拌机，芥茉，甜甜圈，面包皮，洋葱，番茄酱，布丁，开胃酒，鱼子酱，馅饼，肉汤，乳酪'
-wronglst=[]
 wordlst=wordstr.split(',')
 hzlst=hzstr.split('，')
-
+WrongHz = 'Summary1:'
+WrongWord = '------------------------------------------'
 score = 0
 # the location of the 2 lists.
 R=0
+StartFlag = 0
 # the game time left, initially 60 seconds.
 timeleft = 60
 
@@ -32,25 +33,32 @@ def nextWord():
     # and 'play' variables above.
     global score
     global timeleft
-    global R
+    global R,StartFlag,WrongWord,WrongHz
 
     # if a game is currently in play
     if timeleft > 0:
-
+        StartFlag += 1
         # make the text entry box active.
         e.focus_set()
+        if StartFlag==1:
+            R = random.randint(0, len(wordlst) - 1)
+            label.config(fg=str('red'), text=str(StartFlag)+':'+str(hzlst[R]))
+        else:
+            if e.get().lower() == wordlst[R].lower():
+                score += 1
+            else:
+                WrongHz=hzlst[R]
+                WrongWord=wordlst[R]
 
-        if e.get().lower() == wordlst[R].lower():
-            score += 1
+            # clear the text entry box.
+            e.delete(0, tkinter.END)
 
-        # clear the text entry box.
-        e.delete(0, tkinter.END)
+            R=random.randint(0,len(wordlst)-1)
+            label.config(fg=str('red'), text=str(StartFlag)+':'+str(hzlst[R]))
 
-        R=random.randint(0,len(wordlst)-1)
-        label.config(fg=str('red'), text=str(hzlst[R]))
-
-        # update the score.
-        scoreLabel.config(text="Score: " + str(score))
+            # update the score.
+            scoreLabel.config(text="Score: " + str(score))
+            SummaryLabel1.config(text=WrongHz+':'+WrongWord)
 
 # Countdown timer function
 def countdown():
@@ -77,7 +85,7 @@ root = tkinter.Tk()
 root.title("TYPING GAME")
 
 # set the size
-root.geometry("375x250")
+root.geometry("375x270")
 
 # add an instructions label
 instructions = tkinter.Label(root, text="", font=('Helvetica', 12))
@@ -94,7 +102,7 @@ timeLabel = tkinter.Label(root, text="Time left: " +
 timeLabel.pack()
 
 # add a label for displaying the colours
-label = tkinter.Label(root, font=('Helvetica', 48))
+label = tkinter.Label(root, font=('Helvetica', 36))
 label.pack()
 
 # add a text entry box for
@@ -105,6 +113,18 @@ e = tkinter.Entry(root)
 # when the enter key is pressed
 root.bind('<Return>', startGame)
 e.pack()
+
+
+
+BlankLine = tkinter.Label(root, text="", font=('Helvetica', 12))
+BlankLine.pack()
+
+SummaryLabel1= tkinter.Label(root, text="Summary1:------------------------------------------",fg=str('blue'),
+                           font=('Helvetica', 12))
+SummaryLabel1.pack()
+SummaryLabel2= tkinter.Label(root, text="Summary2:------------------------------------------",fg=str('blue'),
+                           font=('Helvetica', 12))
+SummaryLabel2.pack()
 
 # set focus on the entry box
 e.focus_set()
